f7c124eada283380ec4ce6a4e33a5ae8bc177f02
Émile Fugulin <emile@caido.io>
2024-07-20 21:21:40 -0400

    Distribution + cli vs desktop (#112)
    * Add athena os
    
    * Add cli vs desktop
    
    * Optimised images with calibre/image-actions
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- .vitepress/sidebars/concepts.ts
+++ .vitepress/sidebars/concepts.ts
@@ -1,60 +1,69 @@
-import {
-    DefaultTheme
-} from "vitepress";
+import { DefaultTheme } from "vitepress";
 
-export const conceptsSidebar: DefaultTheme.SidebarItem[] = [{
-        text: "Introduction",
-        items: [{
-            text: 'Directory',
-            link: '/concepts/'
-        }]
-    },
-    {
-        text: 'Essentials',
-        items: [{
-                text: 'Instances',
-                link: '/concepts/essentials/instances'
-            },
-            {
-                text: 'HTTPQL',
-                link: '/concepts/essentials/httpql'
-            },
-            {
-                text: 'Workflows',
-                link: '/concepts/essentials/workflows',
-                items: [{
-                    text: 'Nodes',
-                    link: '/concepts/essentials/workflows/nodes/nodes'
-                },
-                {
-                    text: 'JavaScript in Caido',
-                    link: '/concepts/essentials/workflows/js_in_caido'
-            }]
-            },
-            {
-                text: 'Tabs',
-                link: '/concepts/essentials/tabs'
-            },
-            {
-                text: 'Layout',
-                link: '/concepts/essentials/layout'
-            }
-        ]
-    },
-    {
-        text: 'Internals',
-        items: [{
-                text: 'Files',
-                link: '/concepts/internals/files'
-            },
-            {
-                text: 'Cloud',
-                link: '/concepts/internals/cloud'
-            },
-            {
-                text: 'Authentication',
-                link: '/concepts/internals/authentication'
-            }
-        ]
-    },
-]
+export const conceptsSidebar: DefaultTheme.SidebarItem[] = [
+  {
+    text: "Introduction",
+    items: [
+      {
+        text: "Directory",
+        link: "/concepts/",
+      },
+    ],
+  },
+  {
+    text: "Essentials",
+    items: [
+      {
+        text: "CLI vs Desktop",
+        link: "/concepts/essentials/cli_vs_desktop",
+      },
+      {
+        text: "Instances",
+        link: "/concepts/essentials/instances",
+      },
+      {
+        text: "HTTPQL",
+        link: "/concepts/essentials/httpql",
+      },
+      {
+        text: "Workflows",
+        link: "/concepts/essentials/workflows",
+        items: [
+          {
+            text: "Nodes",
+            link: "/concepts/essentials/workflows/nodes/nodes",
+          },
+          {
+            text: "JavaScript in Caido",
+            link: "/concepts/essentials/workflows/js_in_caido",
+          },
+        ],
+      },
+      {
+        text: "Tabs",
+        link: "/concepts/essentials/tabs",
+      },
+      {
+        text: "Layout",
+        link: "/concepts/essentials/layout",
+      },
+    ],
+  },
+  {
+    text: "Internals",
+    items: [
+      {
+        text: "Files",
+        link: "/concepts/internals/files",
+      },
+      {
+        text: "Cloud",
+        link: "/concepts/internals/cloud",
+      },
+      {
+        text: "Authentication",
+        link: "/concepts/internals/authentication",
+      },
+    ],
+  },
+];
--- .vitepress/sidebars/quickstart.ts
+++ .vitepress/sidebars/quickstart.ts
@@ -1,69 +1,90 @@
-import {
-    DefaultTheme
-} from "vitepress";
+import { DefaultTheme } from "vitepress";
 
-export const quickstartSidebar: DefaultTheme.SidebarItem[] = [{
-        text: "Welcome",
-        items: [{
-                text: 'Introduction',
-                link: '/quickstart/'
-        },
-          { text: "Showcase", link: "/quickstart/showcase" },
-                                ]
-    },
-    {
-        text: "Beginner Guide",
+export const quickstartSidebar: DefaultTheme.SidebarItem[] = [
+  {
+    text: "Welcome",
+    items: [
+      {
+        text: "Introduction",
+        link: "/quickstart/",
+      },
+      { text: "Showcase", link: "/quickstart/showcase" },
+    ],
+  },
+  {
+    text: "Beginner Guide",
+    items: [
+      {
+        text: "Welcome to Caido",
         items: [
           {
-            text: "Welcome to Caido",
-            items: [{
-              text: "Onboarding",
-              link: "/quickstart/beginner_guide/welcome_to_caido/onboarding",
-            }]
+            text: "Onboarding",
+            link: "/quickstart/beginner_guide/welcome_to_caido/onboarding",
           },
-            {
-                text: 'Setup',
-                items: [{
-                        text: 'Downloading and Installing',
-                        link: '/quickstart/beginner_guide/setup/install'
-                    },
-                    {
-                        text: 'Basic Configuration',
-                        link: '/quickstart/beginner_guide/setup/config'
-                    },
-                    {
-                        text: 'Uploading Files for Use in Automate',
-                        link: '/quickstart/beginner_guide/setup/file_upload'
-                    }
-                ]
-            },
-            {
-                text: 'First Steps with Caido',
-                items: [{
-                        text: 'Creating Your First Project',
-                        link: '/quickstart/beginner_guide/first_steps_with_caido/project'
-                    },
-                    {
-                        text: 'Capturing and Inspecting Traffic',
-                        link: '/quickstart/beginner_guide/first_steps_with_caido/traffic'
-                    },
-                    {
-                        text: 'Forwarding and Modifying Requests Repeatedly',
-                        link: '/quickstart/beginner_guide/first_steps_with_caido/replay'
-                    },
-                    {
-                        text: 'Automating Brute-Forcing/Fuzzing',
-                        link: '/quickstart/beginner_guide/first_steps_with_caido/automate'
-                    }
-                ]
-            },
-            {
-                text: 'Support',
-                items: [{
-                    text: 'Common Errors and Solutions',
-                    link: '/quickstart/beginner_guide/getting_help/support'
-                }]
-            }
-        ]
-    },
-]
+        ],
+      },
+      {
+        text: "Setup",
+        items: [
+          {
+            text: "Downloading and Installing",
+            link: "/quickstart/beginner_guide/setup/install",
+          },
+          {
+            text: "Basic Configuration",
+            link: "/quickstart/beginner_guide/setup/config",
+          },
+          {
+            text: "Uploading Files for Use in Automate",
+            link: "/quickstart/beginner_guide/setup/file_upload",
+          },
+        ],
+      },
+      {
+        text: "First Steps with Caido",
+        items: [
+          {
+            text: "Creating Your First Project",
+            link: "/quickstart/beginner_guide/first_steps_with_caido/project",
+          },
+          {
+            text: "Capturing and Inspecting Traffic",
+            link: "/quickstart/beginner_guide/first_steps_with_caido/traffic",
+          },
+          {
+            text: "Forwarding and Modifying Requests Repeatedly",
+            link: "/quickstart/beginner_guide/first_steps_with_caido/replay",
+          },
+          {
+            text: "Automating Brute-Forcing/Fuzzing",
+            link: "/quickstart/beginner_guide/first_steps_with_caido/automate",
+          },
+        ],
+      },
+      {
+        text: "Support",
+        items: [
+          {
+            text: "Common Errors and Solutions",
+            link: "/quickstart/beginner_guide/getting_help/support",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: "Distributions",
+    link: "/quickstart/distributions",
+    items: [
+      {
+        text: "Linux",
+        items: [
+          {
+            text: "Athena OS",
+            link: "/quickstart/distributions/linux/athena_os",
+          },
+        ],
+      },
+    ],
+  },
+];
