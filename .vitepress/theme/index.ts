eb1aa00339d72d4544d69e525d92e8723c778151
Emile Fugulin <code@efugulin.com>
2025-03-10 16:44:26 -0400

    Add mermaid

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,11 +2,13 @@ import DefaultTheme from "vitepress/theme";
 import "./custom.css";
 
 import ProContainer from "../components/Pro.vue";
+import Mermaid from "../components/Mermaid.vue";
 import type { Theme } from "vitepress";
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component("ProContainer", ProContainer);
+    app.component("Mermaid", Mermaid);
   },
 } satisfies Theme;
