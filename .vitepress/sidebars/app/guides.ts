acabf050d3495e8724894b4302befcf72ddf8b5c
Ninjeeter <ninjeeter@proton.me>
2026-05-12 12:53:24 -0700

    Guide on setting AI API keys.

--- .vitepress/sidebars/app/guides.ts
+++ .vitepress/sidebars/app/guides.ts
@@ -38,6 +38,10 @@ export const guidesSidebar: DefaultTheme.SidebarItem[] = [
     text: "Configuration",
     items: [
       {
+        text: "Adding AI Provider API Keys",
+        link: "/app/guides/ai",
+      },
+      {
         text: "CA Certificate Management",
         link: "/app/guides/ca_certificate_managing",
       },
